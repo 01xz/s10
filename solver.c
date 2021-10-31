9f7eb88f59a09662a9b7fa6c3a46587ecb05fbf3
01xz <liam.lee@email.com>
2021-10-31 10:27:25 +0800

    update

--- solver.c
+++ solver.c
@@ -1,8 +1,68 @@
 #include <stdio.h>
-#include <>
+#include <cholmod.h>
+#include <time.h>
 
-int main(int argc, char ** argv) {
-  int A[][];
+#define DEBUG
+
+int main(void) {
+  FILE * f_A, * f_b;
+
+  // defination
+  cholmod_sparse * A, * b;
+  cholmod_dense * x;
+  cholmod_factor * L;
+  cholmod_common c;
+
+#ifdef DEBUG
+  // measure the running time
+  clock_t startt, endt;
+  startt = clock();
+#endif
+
+  // start cholmod
+  cholmod_start(&c);
+
+  // fill A and b
+  if ((f_A = fopen ("data_A.mtx", "r")) != NULL) {
+    A = cholmod_read_sparse(f_A, &c);
+  } else {
+    cholmod_free_sparse(&A, &c);
+    cholmod_free_sparse(&b, &c);
+    cholmod_finish(&c);
+    printf("error while reading A.\n");
+    return 0;
+  }
+
+  if ((f_b = fopen ("data_b.mtx", "r")) != NULL) {
+    b = cholmod_read_sparse(f_b, &c);
+  } else {
+    cholmod_free_sparse(&A, &c);
+    cholmod_free_sparse(&b, &c);
+    cholmod_finish(&c);
+    printf("error while reading b.\n");
+    return 0;
+  }
+
+  // solve
+  L = cholmod_analyze(A, &c);
+  cholmod_factorize(A, L, &c);
+  x = cholmod_solve(CHOLMOD_A, L, b, &c);
+
+  // print x
+  cholmod_print_dense(x, "x", &c);
+
+  // finish cholmod
+  cholmod_free_factor(&L, &c);
+  cholmod_free_sparse(&A, &c);
+  cholmod_free_sparse(&b, &c);
+  cholmod_free_dense(&x, &c);
+  cholmod_finish(&c);
+
+#ifdef DEBUG
+  endt = clock();
+  printf("\ntime: %fs\n", (double) (endt - startt) / CLOCKS_PER_SEC);
+#endif
+
   return 0;
 }
 
