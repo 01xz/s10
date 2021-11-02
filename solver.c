4189988c7d762e9a1a7b9381e67ac4eb3cd16272
01xz <liam.lee@email.com>
2021-11-02 09:37:19 +0800

    update

--- solver.c
+++ solver.c
@@ -5,19 +5,13 @@
 #define DEBUG
 
 int main(void) {
-  FILE * f_A, * f_b;
+  FILE * f_A, * f_b, * f_x;
 
   // defination
   cholmod_sparse * A;
   cholmod_dense * x, * b;
   cholmod_factor * L;
   cholmod_common c;
-
-#ifdef DEBUG
-  // measure the running time
-  clock_t startt, endt;
-  startt = clock();
-#endif
 
   // start cholmod
   cholmod_start(&c);
@@ -26,6 +20,7 @@ int main(void) {
   if ((f_A = fopen ("data_A.mtx", "r")) != NULL) {
     printf("reading A:\n");
     A = cholmod_read_sparse(f_A, &c);
+    cholmod_print_sparse(A, "A", &c);
   } else {
     cholmod_free_sparse(&A, &c);
     cholmod_finish(&c);
@@ -36,6 +31,7 @@ int main(void) {
   if ((f_b = fopen ("data_b.mtx", "r")) != NULL) {
     printf("reading b:\n");
     b = cholmod_read_dense(f_b, &c);
+    cholmod_print_dense(b, "b", &c);
   } else {
     cholmod_free_sparse(&A, &c);
     cholmod_free_dense(&b, &c);
@@ -44,13 +40,31 @@ int main(void) {
     return 0;
   }
 
+  // print x
+  if ((f_x = fopen ("data_x.mtx", "w")) != NULL) {
+    printf("writing x:\n");
+  } else {
+    printf("error while saving x.\n");
+  }
+
+#ifdef DEBUG
+  // measure the running time
+  clock_t startt, endt;
+  startt = clock();
+#endif
+
   // solve
   L = cholmod_analyze(A, &c);
   cholmod_factorize(A, L, &c);
   x = cholmod_solve(CHOLMOD_A, L, b, &c);
 
-  // print x
+#ifdef DEBUG
+  endt = clock();
+  printf("\ntime: %fs\n", (double) (endt - startt) / CLOCKS_PER_SEC);
+#endif
+
   cholmod_print_dense(x, "x", &c);
+  cholmod_write_dense(f_x, x, "", &c);
 
   // finish cholmod
   cholmod_free_factor(&L, &c);
@@ -58,11 +72,6 @@ int main(void) {
   cholmod_free_dense(&b, &c);
   cholmod_free_dense(&x, &c);
   cholmod_finish(&c);
-
-#ifdef DEBUG
-  endt = clock();
-  printf("\ntime: %fs\n", (double) (endt - startt) / CLOCKS_PER_SEC);
-#endif
 
   return 0;
 }
